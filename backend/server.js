bd0080b11e4deab32366a5f03c9d584215e98cce
Ram Sthapit <ramsthapit50@gmail.com>
2021-07-12 22:58:04 +0545

    ES Modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import  products from './data/products.js'
 
 dotenv.config()
 
@@ -21,4 +21,4 @@ app.get('/api/products/:id', (req, res) => {
  
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
