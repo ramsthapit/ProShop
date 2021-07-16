215886fb61ab0c1b0fdc5c923c19a2f34b393e29
Ram Sthapit <ramsthapit50@gmail.com>
2021-07-16 18:43:06 +0545

    productDEtails Reducer & Action

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,7 +10,7 @@ import Product from '../models/productModel.js'
 // @access Public 
 router.get('/', asyncHandler(async (req, res) => {
   const products = await Product.find({})
-
+ 
   res.json(products)
 }))
 
