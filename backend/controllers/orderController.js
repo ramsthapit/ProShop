e9a636d42e2c7e055b20beccdc6741809f9d71c1
Ram Sthapit <ramsthapit50@gmail.com>
2021-07-27 19:25:14 +0545

    Order Screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,7 +33,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @routes GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name', 'email')
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
 
   if (order) {
     res.json(order)
