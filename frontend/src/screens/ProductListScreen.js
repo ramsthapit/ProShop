1f6c1efcc3124d44cf2ade91dea624fb68bab0f6
Ram Sthapit <ramsthapit50@gmail.com>
2021-10-09 10:01:37 +0545

    Admin Create Product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,8 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts,deleteProduct } from '../actions/productAction'
+import { listProducts,deleteProduct, createProduct } from '../actions/productAction'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch()
@@ -14,17 +15,26 @@ const ProductListScreen = ({ history, match }) => {
 
   const productDelete = useSelector(state => state.productDelete)
   const { loading:loadingDelete, error:errorDelete, success: successDelete } = productDelete
+  
+  const productCreate = useSelector(state => state.productCreate)
+  const { loading:loadingCreate, error:errorCreate, success: successCreate,product: createdProduct } = productCreate
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts())
-    } else {
+    dispatch({ type: PRODUCT_CREATE_RESET })
+    
+    if (!userInfo.isAdmin) {
       history.push('/login')
     }
-  }, [dispatch, userInfo, history, successDelete ])
+
+    if (successCreate) {
+      history.push(`product/${createdProduct._id}/edit`)
+    } else {
+      dispatch(listProducts())
+    }
+  }, [dispatch, userInfo, history, successDelete, successCreate, createdProduct ])
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
@@ -32,8 +42,8 @@ const ProductListScreen = ({ history, match }) => {
     }
   }
 
-  const createProductHandler = (product) => {
-    // 
+  const createProductHandler = () => {
+    dispatch(createProduct()) 
   }
 
   return (
@@ -49,7 +59,10 @@ const ProductListScreen = ({ history, match }) => {
         </Col>
       </Row>
       {loadingDelete && <Loader />}
-      {errorDelete && <Message variant='danger'>{errorDelete}</Message> }
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+      
       {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>
         : (
           <Table striped bordered hover responsive className='table-sm'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers,applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension' 
-import { productDetailsReducer, productListReducer } from './reducers/productReducers'
+import { productCreateReducer, productDeleteReducer, productDetailsReducer, productListReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userDeleteReducer, userDetailsReducer, userListReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducers'
@@ -9,7 +9,8 @@ import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayRe
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  productDelete: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
