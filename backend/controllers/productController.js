55ca0c6e8f8938adfa831a0f53927421dca47c3b
Ram Sthapit <ramsthapit50@gmail.com>
2021-10-12 17:16:35 +0545

    Product Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,17 +5,19 @@ import Product from '../models/productModel.js'
 // @routes GET /api/products
 // @access Public 
 const getProducts = asyncHandler(async (req, res) => {
-  
+  const pageSize = 10
+  const page = Number(req.query.pageNumber) || 1
+
   const keyword = req.query.keyword ? {
     name: {
       $regex: req.query.keyword,
       $options: 'i'
     }
   }:{}
-    
-  const products = await Product.find({...keyword})
+  const count = await Product.countDocuments({...keyword})
+  const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1))
  
-  res.json(products)
+  res.json({products, page, pages: Math.ceil(count / pageSize) })
 })
 
 // @desc fetch all products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,12 +33,15 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} exact />
+          <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
